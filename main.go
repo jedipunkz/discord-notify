175d473f577a43c7e6ad8d3f53d2a189ad1524b6
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2019-12-28 23:49:33 +0900

    add response to ping message

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func main() {
 	}
 
 	discord.AddHandler(onVoiceStateUpdate)
+	discord.AddHandler(onMessageCreate)
 
 	err = discord.Open()
 	if err != nil {
@@ -76,4 +77,16 @@ func onVoiceStateUpdate(s *discordgo.Session, vs *discordgo.VoiceStateUpdate) {
 	}
 
 	usermap[vs.UserID].CurrentVC = vs.ChannelID
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		return
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	fmt.Printf("Message: %+v\n", m.Message)
 }
